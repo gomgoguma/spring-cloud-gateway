953fe260718a756779a71c108609d87f4fad7fd0
gomgoguma <kebhse@daum.net>
2023-12-03 17:45:14 +0900

    spring cloud gateway

---
+++ src/test/java/com/example/scg/SpringCloudGatewayTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
